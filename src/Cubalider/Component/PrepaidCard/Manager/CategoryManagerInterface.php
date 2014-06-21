6a9f4767027da6f752676a06ed92697cf8c156f0
Yosmany Garcia <yosmanyga@gmail.com>
2014-06-21 10:40:09 -0400

    Added methods to add, pick and remove category

--- src/Cubalider/Component/PrepaidCard/Manager/CategoryManagerInterface.php
+++ src/Cubalider/Component/PrepaidCard/Manager/CategoryManagerInterface.php
@@ -10,10 +10,37 @@ use Cubalider\Component\PrepaidCard\Model\Category;
 interface CategoryManagerInterface
 {
     /**
+     * Adds given category.
+     *
+     * @api
+     * @param Category $category
+     * @return void
+     */
+    public function add(Category $category);
+
+    /**
+     * Picks a category using given criteria
+     *
+     * @api
+     * @param mixed $criteria
+     * @return Category The category
+     */
+    public function pick($criteria);
+
+    /**
      * Gets all categories.
      *
      * @api
      * @return Category[]
      */
     public function collect();
+
+    /**
+     * Removes given category.
+     *
+     * @api
+     * @param Category $category
+     * @return void
+     */
+    public function remove(Category $category);
 }
